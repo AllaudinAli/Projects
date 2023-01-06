e5d7a86706f4472f4c2618dfbf326cdc933906a5
AllaudinAli <ahmadcrisp@gmail.com>
2023-01-06 21:09:44 +0500

    updating

--- MongooseBasics/index.js
+++ MongooseBasics/index.js
@@ -1,3 +1,4 @@
+//Connecting to MongoDB
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://0.0.0.0:27017/movieApp', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -8,13 +9,23 @@ mongoose.connect('mongodb://0.0.0.0:27017/movieApp', { useNewUrlParser: true, us
         console.log("OH NO ERROR")
         console.log(err);
     })
-
+//Creating Schema
 const movieSchema = new mongoose.Schema({
     title: String,
     year: Number,
     score: Number,
     rating: String
 });
-
+//Assinging Schema
 const Movie = mongoose.model('Movie', movieSchema);
-const amadeus = new Movie({title: 'Amadeus', year: 1986, score: 9.2, rating: 'R'})
+//Inserting into Schema
+// Movie.insertMany([
+//     { title: 'Amelia', year: 2001, score: 8.3, rating: 'R' },
+//     { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
+//     { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
+//     { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
+//     { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' },
+// ]).then(data => {
+//     console.log('IT WORKED!')
+//     console.log(data);
+// })
