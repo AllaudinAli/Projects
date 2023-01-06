a72de55e169c09610db7ef897875a100a65cbc79
AllaudinAli <ahmadcrisp@gmail.com>
2023-01-06 20:27:25 +0500

    mongoose model

--- MongooseBasics/index.js
+++ MongooseBasics/index.js
@@ -8,3 +8,13 @@ mongoose.connect('mongodb://0.0.0.0:27017/movieApp', { useNewUrlParser: true, us
         console.log("OH NO ERROR")
         console.log(err);
     })
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+const amadeus = new Movie({title: 'Amadeus', year: 1986, score: 9.2, rating: 'R'})
