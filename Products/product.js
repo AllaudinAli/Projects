0cda1ed92694c30af647581496e22c66eb765be4
AllaudinAli <ahmadcrisp@gmail.com>
2023-01-07 19:47:52 +0500

    product update

--- Products/product.js
+++ Products/product.js
@@ -8,3 +8,52 @@ mongoose.connect('mongodb://0.0.0.0:27017/shopApp', { useNewUrlParser: true, use
         console.log("OH NO ERROR")
         console.log(err);
     })
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: [0, 'Price must be a Positive integer']
+    },
+    onSale: {
+        type: Boolean,
+        default: false
+    },
+    categories: [String],
+    qty: {
+        online: {
+            type: Number,
+            default: 0
+        },
+        inStore: {
+            type: Number,
+            default: 0
+        }
+    },
+    size: {
+        type: String,
+        enum: ['S', 'M', 'L', 'XL']
+    }
+})
+
+const Product = mongoose.model('Product', productSchema);
+const bike = new Product({ name: 'BMX Jersey', price: 349.99, categories: ['Cycling'], size: 'L'})
+bike.save().then(data => {
+    console.log("It Worked!")
+    console.log(data)
+}).catch(err => {
+    console.log('Oh no Error!')
+    console.log(err.errors.name.properties.message);
+})
+
+// Product.findOneAndUpdate({ name: 'Tire Pump' }, { price: 2.99 }, { new: true, runValidators: true })
+//     .then(data => {
+//         console.log("It Worked!")
+//         console.log(data)
+//     }).catch(err => {
+//         console.log('Oh no Error!')
+//         console.log(err);
+//     })
