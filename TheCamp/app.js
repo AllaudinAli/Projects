f102e4b1a7f40d77935213c886c7775f1a7ec9d7
AllaudinAli <ahmadcrisp@gmail.com>
2023-02-15 20:37:49 +0500

    connecting to databases

--- TheCamp/app.js
+++ TheCamp/app.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+//Mongoose Connection
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb://0.0.0.0:27017/the-camp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once("open", () => {
+    console.log("Database Connected");
+});
 
+//Ejs Setup
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
-
-
-
-
-
-app.get('/', (req, res)=>{
+//Web Page
+app.get('/', (req, res) => {
     res.render('home')
 })
 
