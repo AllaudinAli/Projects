dba1bfe5cbe114f508b59814bd8c7ed8c1ae1a44
AllaudinAli <ahmadcrisp@gmail.com>
2023-03-17 16:30:16 +0500

    yea

--- TheCamp/app.js
+++ TheCamp/app.js
@@ -69,6 +69,16 @@ app.delete('/campgrounds/:id', async (req, res) => {
     res.redirect('/campgrounds')
 })
 
+//Errors
+app.all('*', (req, res, next)=>{
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next)=>{
+    const {statusCode = 500} = err;
+    if(!err.message) err.message = 'Oh No, Something went wrong!'
+    res.status(statusCode).render('error', {err})
+})
 
 
 //----------------------------------------------------------------------------------
