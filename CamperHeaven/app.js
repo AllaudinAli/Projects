2e29ec6498992d8f2bf83e8b246cc48c286ff699
AllaudinAli <ahmadcrisp@gmail.com>
2023-05-05 21:20:08 +0500

    Did some styling for review panel

--- CamperHeaven/app.js
+++ CamperHeaven/app.js
@@ -79,7 +79,8 @@ app.post('/campgrounds', ValidationCampground, catchAsync(async (req, res) => {
 
 //Show Campgrounds by ID
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
+    const campground = await Campground.findById(req.params.id).populate('reviews');
+    console.log(campground)
     res.render('campgrounds/show', { campground });
 }))
 
@@ -104,7 +105,7 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
 //Reviews
 app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body);
+    const review = new Review(req.body.review);
     campground.reviews.push(review);
     await review.save();
     await campground.save();
