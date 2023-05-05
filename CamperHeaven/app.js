52c357f11d7a267e90707e74f79ae6297676afc3
AllaudinAli <ahmadcrisp@gmail.com>
2023-05-05 23:12:13 +0500

    Deleting the review along with the campgrounds

--- CamperHeaven/app.js
+++ CamperHeaven/app.js
@@ -80,7 +80,6 @@ app.post('/campgrounds', ValidationCampground, catchAsync(async (req, res) => {
 //Show Campgrounds by ID
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
-    console.log(campground)
     res.render('campgrounds/show', { campground });
 }))
 
@@ -110,6 +109,15 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res)
     await review.save();
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
+}))
+
+//Deleting Reviews
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { review: reviewId } })
+    await Review.findByIdAndDelete(req.params.reviewId);
+    res.redirect(`/campgrounds/${id}`);
 }))
 
 //Errors
