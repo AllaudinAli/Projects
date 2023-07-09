47db5697cb46c1d2aae7991c940741f374156c65
AllaudinAli <ahmadcrisp@gmail.com>
2023-07-09 23:01:01 +0500

    adding controllers

--- CamperHeaven/routes/reviews.js
+++ CamperHeaven/routes/reviews.js
@@ -3,6 +3,7 @@ const router = express.Router({ mergeParams: true });
 const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const Review = require('../models/review');
+const reviews = require('../controllers/reviews')
 const ExpressError = require('../utils/ExpressError');
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 
@@ -11,16 +12,7 @@ const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 //----------------------------------------------------------------------------------
 
 //Reviews
-router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash('success', 'Created new review!')
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview))
 
 //Deleting Reviews
 router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req, res) => {
