c7475b76213fc997085b4f28b89aecf1a31226b8
AllaudinAli <ahmadcrisp@gmail.com>
2023-07-09 22:25:17 +0500

    adding authorization

--- CamperHeaven/routes/campgrounds.js
+++ CamperHeaven/routes/campgrounds.js
@@ -1,24 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
-const { campgroundSchema } = require('../schemas.js');
-const { isLoggedIn } = require('../middleware')
-//----------------------------------------------------------------------------------
-
-const ValidationCampground = (req, res, next) => {
-
-    const { error } = campgroundSchema.validate(req.body)
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    }
-    else {
-        next();
-    }
-}
-
+const { isLoggedIn, isAuthor, validationCampground } = require('../middleware')
 //----------------------------------------------------------------------------------
 
 router.get('/', catchAsync(async (req, res) => {
@@ -30,9 +14,9 @@ router.get('/', catchAsync(async (req, res) => {
 router.get('/new', isLoggedIn, (req, res) => {
     res.render('campgrounds/new');
 })
-router.post('/', isLoggedIn, ValidationCampground, catchAsync(async (req, res) => {
-    // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400)
+router.post('/', isLoggedIn, validationCampground, catchAsync(async (req, res) => {
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
     req.flash('success', 'Successfully made a new Campground!')
     res.redirect(`/campgrounds/${campground._id}`)
@@ -40,7 +24,12 @@ router.post('/', isLoggedIn, ValidationCampground, catchAsync(async (req, res) =
 
 //Show Campgrounds by ID
 router.get('/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews');
+    const campground = await Campground.findById(req.params.id).populate({
+        path: 'reviews',
+        populate: {
+            path: 'author'
+        }
+    }).populate('author');
     if (!campground) {
         req.flash('error', 'We cannot seem to find that :(');
         return res.redirect('/campgrounds');
@@ -49,15 +38,16 @@ router.get('/:id', catchAsync(async (req, res) => {
 }))
 
 //Editing the Campgrounds
-router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id)
     if (!campground) {
         req.flash('error', 'We cannot seem to find that :(');
         return res.redirect('/campgrounds');
     }
     res.render('campgrounds/edit', { campground });
 }))
-router.put('/:id', isLoggedIn, ValidationCampground, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, isAuthor, validationCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     req.flash('success', 'Successfully updated campground!')
@@ -65,8 +55,9 @@ router.put('/:id', isLoggedIn, ValidationCampground, catchAsync(async (req, res)
 }))
 
 //Deleting Campgrounds
-router.delete('/:id', isLoggedIn, catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
     const { id } = req.params;
+
     await Campground.findByIdAndDelete(id);
     req.flash('success', 'Successfully deleted campground')
     res.redirect('/campgrounds')
--- CamperHeaven/seeds/index.js
+++ CamperHeaven/seeds/index.js
@@ -22,6 +22,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20000) + 1000;
         const camp = new Campground({
+            author: '64a9894526add6083c995c51',
             location: `${cities[random1000].city},${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
