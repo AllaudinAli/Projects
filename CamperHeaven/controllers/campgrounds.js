f42ed619bb9081e4256467525d473f6eff4180e8
AllaudinAli <ahmadcrisp@gmail.com>
2023-07-16 15:38:24 +0500

    added mapbox-sdk

--- CamperHeaven/controllers/campgrounds.js
+++ CamperHeaven/controllers/campgrounds.js
@@ -1,22 +1,37 @@
 const Campground = require('../models/campground');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken })
 const { cloudinary } = require('../cloudinary');
 
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
 }
+
+
 module.exports.renderNewForm = (req, res) => {
     res.render('campgrounds/new');
 }
+
+
 module.exports.createCampground = async (req, res) => {
-    const campground = new Campground(req.body.campground);
-    campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }))
-    campground.author = req.user._id;
-    await campground.save();
-    console.log(campground)
-    req.flash('success', 'Successfully made a new Campground!')
-    res.redirect(`/campgrounds/${campground._id}`)
+    const geoData = await geocoder.forwardGeocode({
+        query: 'Lahore, Pakistan',
+        limit: 1
+    }).send()
+    console.log(geoData);
+    res.send("OK!")
+    // const campground = new Campground(req.body.campground);
+    // campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }))
+    // campground.author = req.user._id;
+    // await campground.save();
+    // console.log(campground)
+    // req.flash('success', 'Successfully made a new Campground!')
+    // res.redirect(`/campgrounds/${campground._id}`)
 }
+
+
 module.exports.showCampgroundById = async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate({
         path: 'reviews',
@@ -31,6 +46,8 @@ module.exports.showCampgroundById = async (req, res) => {
     }
     res.render('campgrounds/show', { campground });
 }
+
+
 module.exports.renderEditForm = async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id)
@@ -40,6 +57,8 @@ module.exports.renderEditForm = async (req, res) => {
     }
     res.render('campgrounds/edit', { campground });
 }
+
+
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
@@ -55,6 +74,8 @@ module.exports.updateCampground = async (req, res) => {
     req.flash('success', 'Successfully updated campground!')
     res.redirect(`/campgrounds/${campground._id}`);
 }
+
+
 module.exports.deleteCampground = async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
